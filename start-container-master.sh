0fe1170b74ebe136a7a415d6ac34e20ef39e7efe
weiliangteh <weiliangteh@gmail.com>
2024-01-24 20:14:11 +0800

    change port master

--- start-container-master.sh
+++ start-container-master.sh
@@ -6,8 +6,7 @@ sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 sudo docker run -itd \
                 --net=hadoop \
-                -p 50070:50070 \
-                -p 8088:8088 \
+                -P \
                 --name hadoop-master \
                 --hostname hadoop-master \
                 william/hadoop &> /dev/null
